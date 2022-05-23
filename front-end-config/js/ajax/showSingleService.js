19841a6d5e0222336a89516dd8fafe5b223c7de6
Jefry16 <jefrycayo16@gmail.com>
2022-05-23 18:21:41 +0200

    f

--- front-end-config/js/ajax/showSingleService.js
+++ front-end-config/js/ajax/showSingleService.js
@@ -7,7 +7,8 @@ export async function showSingleService() {
     const title = route[2].split("-").join(" ");
     const service = dataAsJson.find((s) => s.title === title);
     const pageContainer = document.querySelector(".service");
-    if (service) {
+    if (service && service.title != "autoconsumo") {
+      document.querySelector(".second-img-cont").remove();
       pageContainer.querySelector("h1").textContent =
         service.title.charAt(0).toUpperCase() + service.title.slice(1);
       let description = "";
@@ -51,6 +52,43 @@ export async function showSingleService() {
         pageContainer.querySelector(".fab-title").remove();
         pageContainer.querySelector(".fab").remove();
       }
+    } else if (service && service.title == "autoconsumo") {
+      document.querySelector(".slide-holder").remove();
+      pageContainer.querySelector("h1").textContent = "Autoconsumo";
+
+      let description = "";
+      service.description.forEach((x) => (description += `<p>${x}</p>`));
+      pageContainer.querySelector(".description").innerHTML = description;
+
+      pageContainer.querySelector(".why .why__title").textContent +=
+        "autoconsumo?";
+
+      pageContainer.querySelector(".why .why__desc").textContent = service.why;
+
+      pageContainer
+        .querySelector(".main-image-container img")
+        .setAttribute("src", "/public/images/" + service.main);
+
+      pageContainer
+        .querySelector(".second-img-cont img")
+        .setAttribute("src", `/public/images/${service.second}`);
+
+      let paraNegocio = "";
+      service.paraNegocio.forEach(
+        (x) =>
+          (paraNegocio += `<div class="item"><img class="iconlist" src="/public/images/iconli.svg""> <p>${x}</p></div>`)
+      );
+      pageContainer.querySelector(".paraNegocio").innerHTML += paraNegocio;
+
+      let paraVivienda = "";
+      service.paraVivienda.forEach(
+        (x) =>
+          (paraVivienda += `<div class="item"><img class="iconlist" src="/public/images/iconli.svg""> <p>${x}</p></div>`)
+      );
+      pageContainer.querySelector(".paraVivienda").innerHTML += paraVivienda;
+      pageContainer
+        .querySelector(".second-img-cont .third")
+        .setAttribute("src", `/public/images/${service.third}`);
     }
   }
 }
